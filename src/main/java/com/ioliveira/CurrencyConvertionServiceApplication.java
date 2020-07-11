7d097497d545ad6698ce9bee0bea434b33721910
Igor Oliveira <igorfoliveira@gmail.com>
2020-07-11 15:19:33 -0300

    criacao do projeto

---
+++ src/main/java/com/ioliveira/CurrencyConvertionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ioliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConvertionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConvertionServiceApplication.class, args);
+	}
+
+}
