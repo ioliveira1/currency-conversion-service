7b59fdcf15d06c6fb0ec77c75d0acc23074ef756
Igor Oliveira <igorfoliveira@gmail.com>
2020-07-12 16:52:56 -0300

    configurando Zuul entre chamadas de servicos

--- src/main/java/com/ioliveira/feigns/CurrencyConversionFeignClient.java
+++ src/main/java/com/ioliveira/feigns/CurrencyConversionFeignClient.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyConversionFeignClient {
 
     //Mapeamento do controller CurrencyExchangeController
-    @GetMapping(path = "/exchange/{from}/{to}")
+    @GetMapping(path = "currency-exchange-service/exchange/{from}/{to}")
     public CurrencyConversion getExchangeValue(@PathVariable String from, @PathVariable String to);
 
 }
