28b2180bc0add2e283c42f15ffdfc6d858f20cee
Igor Oliveira <igorfoliveira@gmail.com>
2020-07-12 17:57:26 -0300

    implementando Spring Cloud Sleuth

--- src/main/java/com/ioliveira/CurrencyConversionServiceApplication.java
+++ src/main/java/com/ioliveira/CurrencyConversionServiceApplication.java
@@ -1,9 +1,11 @@
 package com.ioliveira;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,6 +14,12 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+	//Trace de todas as requests
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
